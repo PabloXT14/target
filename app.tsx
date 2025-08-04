b5bd0db54479cbb00639d4c2d9b1422230b02285
Pablo Alan <pabloxt14@gmail.com>
2025-08-04 17:27:17 -0300

    feat: add assets

--- app.tsx
+++ app.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
+import { colors } from '@/theme/colors'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Target App</Text>
       <StatusBar style="auto" />
     </View>
   )
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.blue[400],
     alignItems: 'center',
     justifyContent: 'center',
   },
