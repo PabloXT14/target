eca288dc6a9819d89bd8427e052289482c8d9568
Pablo Alan <pabloxt14@gmail.com>
2025-08-15 17:17:17 -0300

    feat: create connection with database

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { View } from 'react-native'
 import { Stack } from 'expo-router'
 import {
@@ -6,6 +7,10 @@ import {
   Inter_500Medium,
   Inter_700Bold,
 } from '@expo-google-fonts/inter'
+
+import { SQLiteProvider } from 'expo-sqlite'
+
+import { migrate } from '@/database/migrate'
 
 import { colors } from '@/theme/colors'
 
@@ -27,14 +32,18 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: {
-          backgroundColor: colors.white,
-        },
-        animation: 'slide_from_right',
-      }}
-    />
+    <Suspense fallback={<Loading />}>
+      <SQLiteProvider databaseName="target.db" onInit={migrate} useSuspense>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: {
+              backgroundColor: colors.white,
+            },
+            animation: 'slide_from_right',
+          }}
+        />
+      </SQLiteProvider>
+    </Suspense>
   )
 }
