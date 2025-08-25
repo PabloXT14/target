e4c636c29533ecd0d0945f4861af040bc139a11b
Pablo Alan <pabloxt14@gmail.com>
2025-08-25 17:11:25 -0300

    feat: create remove transaction method

--- src/database/use-transactions-database.ts
+++ src/database/use-transactions-database.ts
@@ -41,8 +41,13 @@ export function useTransactionsDatabase() {
     )
   }
 
+  async function remove(id: number) {
+    await database.runAsync('DELETE FROM transactions WHERE id = ?', [id])
+  }
+
   return {
     create,
     findByTargetId,
+    remove,
   }
 }
